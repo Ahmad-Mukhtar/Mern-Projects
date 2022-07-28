96c6a0bdeddcf9aafaba49797d14663d20cd06a0
Ahmad-Mukhtar <66535485+Ahmad-Mukhtar@users.noreply.github.com>
2022-07-28 15:59:35 +0500

    almost done

--- Memories Web App/client/src/App.js
+++ Memories Web App/client/src/App.js
@@ -1,11 +1,51 @@
+import React,{useEffect} from 'react'
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
+import {useDispatch} from 'react-redux'
 import './App.css';
 
+import {getposts} from './actions/posts'
+import memories from './images/memories.png'
+import Posts from './Components/Posts/Posts';
+import Form from './Components/Form/Forms';
+import useSTyles from './styles'
+
+
 function App() {
+
+    const classes=useSTyles();
+    const dispatch=useDispatch();
+
+    useEffect(()=>
+    {
+        dispatch(getposts);
+    },[dispatch])
+
+
     return ( 
-        <div>
-            <h1>hello</h1>
-            
-        </div>
+       <Container maxidth="lg">
+        <AppBar className={classes.appBar} position='static' color='inherit'>
+            <Typography className={classes.heading} variant='h2' align='center'>
+                Memories
+            </Typography>
+            <img className={classes.image} src={memories} alt='memories' height="60"/>
+        </AppBar>
+        <Grow in>
+            <Container>
+            <Grid container  justifyContent="space-between" alignItems="stretch" spacing={3}>
+                <Grid item xs={12} sm={7}>
+                <Posts/>
+                </Grid>
+                
+                <Grid item xs={12} sm={4}>
+                    <Form/>
+                </Grid>
+
+
+            </Grid>
+            </Container>
+        </Grow>
+
+       </Container>
       
     );
 }
